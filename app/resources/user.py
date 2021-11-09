2228c9e66ffe873adacba4eab79a5496267067b1
StefanTrsunov <trsunovstefan@gmail>
2021-11-09 21:06:32 +0100

    Fixing and editing user.py in the resources file

--- app/resources/user.py
+++ app/resources/user.py
@@ -2,9 +2,12 @@ from flask_restful import reqparse
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 
 from flask import current_app as app
-from flask_restx import Namespace
+from flask_restx import Namespace, Resource
 
+from app.helpers.common import authorize, status_code_responses, is_admin
 from app.models.user import User
+
+import hashlib
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument(
@@ -33,3 +36,30 @@ _user_parser.add_argument(
 )
 
 api = Namespace('users', path='/api', description='Users')
+
+@api.route('/register')
+@api.doc(responses=status_code_responses,
+         security=['apitoken']
+        )
+class UserRegister(Resource):
+    def post(self):
+        data = _user_parser.parse_args()
+
+        username = data["username"]
+
+        if User.find_user_by_username(data["username"]):
+            return {
+                    f"message": "User {data[username]} exists!"
+            }, 400
+
+        user = User(username, hashlib.sha256(data["password"].encode("utf-8")).hexdigest(), data["name"], data["email"])
+
+        try:            
+            user.save_to_db()
+            return {
+                    f"message": "User {username} created!"
+            }, 200
+        except:
+            return {
+                    f"message": "The user {username} was not saved in the database."
+            }, 500
