1fe9f7f251c00e0218f70674fcfc983520cd8a35
StefanTrsunov <trsunovstefan@gmail>
2021-12-21 21:39:28 +0100

    Fixing my previous commit

--- tests/resources/test_book.py
+++ tests/resources/test_book.py
@@ -1,9 +1,8 @@
 import json
 
-def books_json(id,title,author,genre,seller):
+def books_json(title,author,genre,seller):
     return json.dumps(
         {
-            "id": id,
             "title": title,
             "author": author,
             "genre": genre,
@@ -14,33 +13,22 @@ def books_json(id,title,author,genre,seller):
 def test_book_should_add(client):
     res = client.post(
         '/api/books',
-        data = books_json("1", "Harry Ptter", "J.K. Rowling", "Adventorious","Bezos"),
+        data = books_json("Harry Ptter", "J.K. Rowling", "Adventorious","Bezos"),
         content_type='application/json'
     )
 
     assert res.status_code == 200
 
-def test_book_should_not_add(client):    
-    
-    res = client.post(
-        '/api/books',
-        data = books_json("321", "Book123", "J.K. Rowling4132", "Comedy/Horror","You"),
-        content_type = 'application/json'
-    )
-
-    assert res.status_code == 400
+def test_book_should_not_add(client):
+    book_json =  {
+        "author": "author",
+        "genre": "genre",
+        "seller": "seller",
+    }
 
     res = client.post(
         '/api/books',
-        data = books_json("22", "WorstBook", "You421", "Comedy","Me413"),
-        content_type = 'application/json'
-    )
-
-    assert res.status_code == 400
-
-    res = client.post(
-        '/api/books',
-        data = books_json("411", "", "", "",""),
+        data = book_json,
         content_type = 'application/json'
     )
 
